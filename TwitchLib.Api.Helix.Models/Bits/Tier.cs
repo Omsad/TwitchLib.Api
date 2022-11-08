4ac78acf151332cf138c56b973b2501b0ccd35bd
Thomas <thomas.lee@occaria.co.uk>
2022-11-08 19:13:48 +0000

    Fix tier merge.

--- TwitchLib.Api.Helix.Models/Bits/Tier.cs
+++ TwitchLib.Api.Helix.Models/Bits/Tier.cs
@@ -4,12 +4,12 @@ namespace TwitchLib.Api.Helix.Models.Bits
 {
     public class Tier
     {
-        [JsonProperty(PropertyName = "color")]
-        public string Color { get; protected set; }
-
+        [JsonProperty(PropertyName = "min_bits")]
+        public int MinBits { get; protected set; }
         [JsonProperty(PropertyName = "id")]
         public string Id { get; protected set; }
-
+        [JsonProperty(PropertyName = "color")]
+        public string Color { get; protected set; }
         [JsonProperty(PropertyName = "images")]
         public Images Images { get; protected set; }
         [JsonProperty(PropertyName = "can_cheer")]
