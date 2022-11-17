a1aba62bc373eb391760fb3c08d779fb9d55df3d
Thomas <thomas.lee@occaria.co.uk>
2022-11-17 17:19:25 +0000

    Update chatters

--- TwitchLib.Api.Helix.Models/Chat/GetChatters/Chatter.cs
+++ TwitchLib.Api.Helix.Models/Chat/GetChatters/Chatter.cs
@@ -5,9 +5,21 @@ namespace TwitchLib.Api.Helix.Models.Chat.GetChatters
     public class Chatter
     {
         /// <summary>
+        /// The identifier of a user that’s connected to the broadcaster’s chat room.
+        /// </summary>
+        [JsonProperty("user_id")]
+        public string UserId { get; protected set; }
+
+        /// <summary>
         /// The login name of a user that’s connected to the broadcaster’s chat room.
         /// </summary>
         [JsonProperty("user_login")]
-        public string UserLogin { get; protected set; }
+        public string UserName { get; protected set; }
+
+        /// <summary>
+        /// The login name of a user that’s connected to the broadcaster’s chat room.
+        /// </summary>
+        [JsonProperty("user_name")]
+        public string DisplayName { get; protected set; }
     }
 }
