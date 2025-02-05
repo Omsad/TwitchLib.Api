bd9bc66312b4d0099216d47eff40443b6ba08b4e
omsad <omsad@instructbot.co.uk>
2025-02-05 17:15:19 +0000

    Bug fixing.

--- TwitchLib.Api.Helix.Models/Predictions/Outcome.cs
+++ TwitchLib.Api.Helix.Models/Predictions/Outcome.cs
@@ -8,6 +8,18 @@ namespace TwitchLib.Api.Helix.Models.Predictions;
 public class Outcome
 {
     /// <summary>
+    /// The number of Channel Points spent by viewers on this outcome.
+    /// </summary>
+    [JsonProperty(PropertyName = "channel_points")]
+    public int ChannelPointsVotes { get; protected set; }
+
+    /// <summary>
+    /// The color that visually identifies this outcome in the UX.
+    /// </summary>
+    [JsonProperty(PropertyName = "color")]
+    public string Color { get; protected set; }
+
+    /// <summary>
     /// An ID that identifies this outcome.
     /// </summary>
     [JsonProperty(PropertyName = "id")]
@@ -20,26 +32,14 @@ public class Outcome
     public string Title { get; protected set; }
 
     /// <summary>
-    /// The number of unique viewers that chose this outcome.
-    /// </summary>
-    [JsonProperty(PropertyName = "users")]
-    public int ChannelPoints { get; protected set; }
-
-    /// <summary>
-    /// The number of Channel Points spent by viewers on this outcome.
-    /// </summary>
-    [JsonProperty(PropertyName = "channel_points")]
-    public int ChannelPointsVotes { get; protected set; }
-
-    /// <summary>
     /// A list of viewers who were the top predictors; otherwise, null if none.
     /// </summary>
     [JsonProperty(PropertyName = "top_predictors")]
     public TopPredictor[] TopPredictors { get; protected set; }
 
     /// <summary>
-    /// The color that visually identifies this outcome in the UX.
+    /// The number of unique viewers that chose this outcome.
     /// </summary>
-    [JsonProperty(PropertyName = "color")]
-    public string Color { get; protected set; }
+    [JsonProperty(PropertyName = "users")]
+    public int Users { get; protected set; }
 }
--- TwitchLib.Api.Helix.Models/Predictions/Prediction.cs
+++ TwitchLib.Api.Helix.Models/Predictions/Prediction.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using TwitchLib.Api.Core.Enums;
 
 namespace TwitchLib.Api.Helix.Models.Predictions;
@@ -9,16 +10,16 @@ namespace TwitchLib.Api.Helix.Models.Predictions;
 public class Prediction
 {
     /// <summary>
-    /// An ID that identifies this prediction.
-    /// </summary>
-    [JsonProperty(PropertyName = "id")]
-    public string Id { get; protected set; }
-
-    /// <summary>
     /// An ID that identifies the broadcaster
     /// </summary>
     [JsonProperty(PropertyName = "broadcaster_id")]
     public string BroadcasterId { get; protected set; }
+
+    /// <summary>
+    /// The broadcaster’s login name.
+    /// </summary>
+    [JsonProperty(PropertyName = "broadcaster_login")]
+    public string BroadcasterLogin { get; protected set; }
 
     /// <summary>
     /// The broadcaster’s display name.
@@ -27,10 +28,47 @@ public class Prediction
     public string BroadcasterName { get; protected set; }
 
     /// <summary>
-    /// The broadcaster’s login name.
+    /// The UTC date and time of when the Prediction began.
     /// </summary>
-    [JsonProperty(PropertyName = "broadcaster_login")]
-    public string BroadcasterLogin { get; protected set; }
+    [JsonProperty(PropertyName = "created_at")]
+    public DateTime CreatedAt { get; protected set; }
+
+    /// <summary>
+    /// The UTC date and time of when the Prediction ended. If status is ACTIVE, this is set to null.
+    /// </summary>
+    [JsonProperty(PropertyName = "ended_at")]
+    public DateTime? EndedAt { get; protected set; }
+
+    /// <summary>
+    /// An ID that identifies this prediction.
+    /// </summary>
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; protected set; }
+
+    /// <summary>
+    /// The UTC date and time of when the Prediction was locked. If status is not LOCKED, this is
+    /// set to null.
+    /// </summary>
+    [JsonProperty(PropertyName = "locked_at")]
+    public DateTime? LockedAt { get; protected set; }
+
+    /// <summary>
+    /// The list of possible outcomes for the prediction.
+    /// </summary>
+    [JsonProperty(PropertyName = "outcomes")]
+    public Outcome[] Outcomes { get; protected set; }
+
+    /// <summary>
+    /// The length of time (in seconds) that the prediction will run for.
+    /// </summary>
+    [JsonProperty(PropertyName = "prediction_window")]
+    public int PredictionWindow { get; protected set; }
+
+    /// <summary>
+    /// The prediction’s status.
+    /// </summary>
+    [JsonProperty(PropertyName = "status")]
+    public PredictionStatus Status { get; protected set; }
 
     /// <summary>
     /// The question that the prediction asks.
@@ -43,40 +81,4 @@ public class Prediction
     /// </summary>
     [JsonProperty(PropertyName = "winning_outcome_id")]
     public string WinningOutcomeId { get; protected set; }
-
-    /// <summary>
-    /// The list of possible outcomes for the prediction.
-    /// </summary>
-    [JsonProperty(PropertyName = "outcomes")]
-    public Outcome[] Outcomes { get; protected set; }
-
-    /// <summary>
-    /// The length of time (in seconds) that the prediction will run for.
-    /// </summary>
-    [JsonProperty(PropertyName = "prediction_window")]
-    public string PredictionWindow { get; protected set; }
-
-    /// <summary>
-    /// The prediction’s status.
-    /// </summary>
-    [JsonProperty(PropertyName = "status")]
-    public PredictionStatus Status { get; protected set; }
-
-    /// <summary>
-    /// The UTC date and time of when the Prediction began.
-    /// </summary>
-    [JsonProperty(PropertyName = "created_at")]
-    public string CreatedAt { get; protected set; }
-
-    /// <summary>
-    /// The UTC date and time of when the Prediction ended. If status is ACTIVE, this is set to null.
-    /// </summary>
-    [JsonProperty(PropertyName = "ended_at")]
-    public string EndedAt { get; protected set; }
-
-    /// <summary>
-    /// The UTC date and time of when the Prediction was locked. If status is not LOCKED, this is set to null.
-    /// </summary>
-    [JsonProperty(PropertyName = "locked_at")]
-    public string LockedAt { get; protected set; }        
 }
